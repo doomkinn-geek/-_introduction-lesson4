8a424f25b7ac5422fd90f6de9d37bda875a51925
doomkinn-geek <doomkinn@yandex.ru>
2021-03-29 19:44:19 +0300

    task 4 done

---
+++ task4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите номер числа: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int output = fibo(input);
+            Console.WriteLine($"Число Фибоначчи номер {input} - это {output}");
+        }
+        public static int fibo(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return fibo(n - 1) + fibo(n - 2);
+            }
+        }
+    }
+}
