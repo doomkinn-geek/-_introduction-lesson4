4c8f31cb7852eee2b18f96d3dc31ef9877dbfcbf
doomkinn-geek <doomkinn@yandex.ru>
2021-03-29 16:59:27 +0300

    task 1 done

--- task1/Program.cs
+++ task1/Program.cs
@@ -6,7 +6,30 @@ namespace task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name, fatherName, patronymic, fio;
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Введите имя: ");
+                name = Console.ReadLine();
+                Console.Write("Введите фамилию: ");
+                fatherName = Console.ReadLine();
+                Console.Write("Введите отчество: ");
+                patronymic = Console.ReadLine();
+                fio = GetFullName(name, fatherName, patronymic);
+                Console.WriteLine($"Полное имя: {fio}");
+            }
+        }
+
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            string _firstName = firstName.Substring(0, 1).ToUpper();
+            _firstName += firstName[1..].ToLower();
+            string _lastName = lastName.Substring(0, 1).ToUpper();
+            _lastName += lastName[1..].ToLower();
+            string _patronymic = patronymic.Substring(0, 1).ToUpper();
+            _patronymic += patronymic[1..].ToLower();
+
+            return _firstName + " " + _lastName + " " + _patronymic;
         }
     }
 }
