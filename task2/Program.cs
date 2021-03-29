c7e086ed62bb071a4e70466db71ab36af58e0805
doomkinn-geek <doomkinn@yandex.ru>
2021-03-29 19:23:49 +0300

    task2 done

--- task2/Program.cs
+++ task2/Program.cs
@@ -6,7 +6,15 @@ namespace task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите числа, разделенные пробелом: ");
+            string numbersStr = Console.ReadLine();
+            string[] nums = numbersStr.Split(' ');
+            int sum = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                sum += Convert.ToInt32(nums[i]);
+            }
+            Console.WriteLine($"Сумма введенных чисел = {sum}");
         }
     }
 }
